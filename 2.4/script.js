35d8f603f84f66a820ccf00f08b265a383241070
Cunning-Fox-99 <kostya.moroz.96@gmail.com>
2022-03-29 10:25:31 +0300

    fist init

---
+++ 2.4/script.js
@@ -0,0 +1,10 @@
+let admin
+let name
+
+name = 'Джон'
+admin = name
+alert(admin)
+
+let planet = 'Земля'
+let user = 'Джон'
+
---
+++ 2.5/script.js
@@ -0,0 +1,7 @@
+let name = "Ilya"
+
+alert( `hello ${1}` ); // Строка с числом
+
+alert( `hello ${"name"}` ) // Строка с текстом name
+
+alert( `hello ${name}` ) // Строка с переменной
